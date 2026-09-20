c3d5c16c47eb5ff5b073bebfe261f49d6d336953
tatsuya0726 <aa047076@gmail.com>
2026-09-20 19:29:35 +0900

    激昂と突進とトリックの画像を更新

--- dist/card-codex.js
+++ dist/card-codex.js
@@ -5,8 +5,8 @@ KEYS.push(...Object.keys(window.ABYSS_ASCENSION_CARDS||{}));
 const PHOTO_ART={
   bettarevenge:'assets/cards/card-betta-revenge.webp',
   zeroshift:'assets/cards/card-zero-shift-v3.webp',surpriseattack:'assets/cards/card-surprise-attack-v5.webp',bloodprice:'assets/cards/card-blood-price.webp',graverobber:'assets/cards/card-grave-robber-v2.webp',heavybomb:'assets/cards/card-heavy-bomb.webp',
-  madness:'assets/cards/card-madness.webp',abysssonar:'assets/cards/card-abyss-sonar.webp',tideforesight:'assets/cards/card-rage-v2.webp',hungryshoal:'assets/cards/card-hungry-shoal.webp',recklesscharge:'assets/cards/card-reckless-charge-v2.webp',abyssescape:'assets/cards/card-abyss-escape.webp',
-  trick:'assets/cards/card-trick.webp',venomfield:'assets/cards/card-venom-field.webp',invisiblehand:'assets/cards/card-invisible-hand.webp',
+  madness:'assets/cards/card-madness.webp',abysssonar:'assets/cards/card-abyss-sonar.webp',tideforesight:'assets/cards/card-rage-v3.webp',hungryshoal:'assets/cards/card-hungry-shoal.webp',recklesscharge:'assets/cards/card-reckless-charge-v3.webp',abyssescape:'assets/cards/card-abyss-escape.webp',
+  trick:'assets/cards/card-trick-v2.webp',venomfield:'assets/cards/card-venom-field.webp',invisiblehand:'assets/cards/card-invisible-hand.webp',
   overdrive:'assets/cards/card-overdrive.webp',
   ink:'assets/cards/plush-ink.webp',
   rampage:'assets/cards/plush-rampage.webp',
